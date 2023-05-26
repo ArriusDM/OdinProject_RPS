33073106c3c262dad4dc9de8ab2e726a93b93d17
ArriusDM <ArriusDM@users.noreply.github.com>
2023-05-25 17:14:08 -0700

    Refactored code to work with button UI

--- script.js
+++ script.js
@@ -3,24 +3,35 @@ const choicesRPS = ["rock", "paper", "scissors"]
 const chooseRock = document.getElementById("rock")
 const choosePaper = document.getElementById("paper")
 const chooseScissors = document.getElementById("scissors")
-const startGame = document.getElementById("start-btn")
-
-let playerScore = 0
-let cpuScore = 0
 
 
-startGame.addEventListener("click", playGame)
+let playerScore = document.getElementById('player-score').innerHTML = 0;
+let cpuScore = document.getElementById('cpu-score').innerHTML = 0;
+let infoText = document.getElementById('info-text')
 
-chooseRock.addEventListener("click", getPlayerChoice())
+let playerSelection = ""
+
+
+chooseRock.addEventListener("click", playerChooseRock)
+choosePaper.addEventListener("click", playerChoosePaper)
+chooseScissors.addEventListener("click", playerChooseScissors)
+
+function playerChooseRock() {
+    playerSelection = "rock"
+    playGame()
+}
+function playerChoosePaper() {
+    playerSelection = "paper"
+    playGame()
+}
+function playerChooseScissors() {
+    playerSelection = "scissors"
+    playGame()
+}
 
 function getComputerChoice() {
     const cpuIndex = Math.floor(Math.random() * choicesRPS.length)
     return computerSelection = choicesRPS[cpuIndex]
-}
-function getPlayerChoice() {
-    let playerPrompt = prompt("Rock, Paper, or Scissors?")
-    playerSelection = playerPrompt.toLowerCase()
-
 }
 
 function playRound(playerchoice, cpuchoice) {
@@ -28,35 +39,62 @@ function playRound(playerchoice, cpuchoice) {
         playerSelection === "paper" && computerSelection === "paper" ||
         playerSelection === "scissors" && computerSelection === "scissors") {
         console.log(`You chose ${playerchoice} and the computer chose ${cpuchoice}, you drew the round!`)
+        document.getElementById('info-text').innerHTML = `You chose ${playerchoice} and the computer chose ${cpuchoice}, you drew the round!`
         return
     } else if (playerSelection === "rock" && computerSelection === "paper" ||
         playerSelection === "paper" && computerSelection === "scissors" ||
         playerSelection === "scissors" && computerSelection === "rock") {
         console.log(`You chose ${playerchoice} and the computer chose ${cpuchoice}, you lost the round!`)
         cpuScore++
+        document.getElementById('cpu-score').innerHTML = cpuScore;
+        document.getElementById('info-text').innerHTML = `You chose ${playerchoice} and the computer chose ${cpuchoice}, you lost the round!`
         return
     } else if (playerSelection === "rock" && computerSelection === "scissors" ||
         playerSelection === "scissors" && computerSelection === "paper" ||
         playerSelection === "paper" && computerSelection === "rock") {
         console.log(`You chose ${playerchoice} and the computer chose ${cpuchoice}, you won the round!`)
         playerScore++
+        document.getElementById('player-score').innerHTML = playerScore;
+        document.getElementById('info-text').innerHTML = `You chose ${playerchoice} and the computer chose ${cpuchoice}, you won the round!`
         return
     }
 
 }
 function playGame() {
-    playerScore = 0
-    cpuScore = 0
-    while (playerScore < 5 && cpuScore < 5) {
-        getPlayerChoice()
+    if (playerScore < 5 && cpuScore < 5) {
         getComputerChoice()
         playRound(playerSelection, computerSelection)
         console.log(playerScore, cpuScore)
         if (playerScore === 5) {
             console.log(`You won the game ${playerScore} to ${cpuScore}!`)
+            document.getElementById('info-text').innerHTML = `You won the game ${playerScore} to ${cpuScore}! Press any button to start another game`
         } else if (cpuScore === 5) {
             console.log(`You lost the game ${playerScore} to ${cpuScore}!`)
+            document.getElementById('info-text').innerHTML = `You lost the game ${playerScore} to ${cpuScore}! Press any button to start another game`
         }
+    } else if (playerScore >= 5 || cpuScore >= 5) {
+        resetGame()
     }
 
 }
+
+function resetGame() {
+    document.getElementById('info-text').innerHTML = `Select Rock, Paper, or Scissors to begin the game`
+    document.getElementById('player-score').innerHTML = 0
+    document.getElementById('cpu-score').innerHTML = 0
+    playerScore = 0
+    cpuScore = 0
+    playerSelection = ""
+    
+}
+
+// function startAndPlay() {
+//     if (playerScore >= 5 || cpuScore >= 5) {
+//         resetGame()
+//     } else {
+//         playGame()
+//     }
+
+// }
+
+
